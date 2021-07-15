82d6a08bd17ca46d1615b64d728596c9bdd9f5d0
Vector¹ <54133395+Vector1dev@users.noreply.github.com>
2021-07-15 17:18:30 +0300

    Refactor code.
    Added function *render* and removed class *Page*

--- FastRenDer/__init__.py
+++ FastRenDer/__init__.py
@@ -16,19 +16,5 @@ class Component():
     def add_Component(self, component):
         self.components.append(component)
 
-
-class Page():
-    def __init__(self, name, file: os.PathLike):
-        self.name = name
-        self.file: os.PathLike = file
-        self.components = []
-
-    def render(self):
-        file = open(str(self.file), "r")
-        renderderer = file.read()
-        for component in self.components:
-            renderderer = renderderer.replace(''.join(["<", "component:", component.name, "/>"]), component.render())
-        return renderderer
-
-    def add_Component(self, component: Component):
-        self.components.append(component)
+def render(comp):
+    return comp.render()
