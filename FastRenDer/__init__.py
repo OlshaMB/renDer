ae18eb6aaa08957947a5cf3b31ffee0763e6df8d
OlshaMB <michaelolshanskiy@icloud.com>
2021-07-15 18:33:22 +0300

    Refractor code ·Removed some code

--- FastRenDer/__init__.py
+++ FastRenDer/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 
-class Component():
+class Component:
     def __init__(self, name, file: os.PathLike):
         self.name = name
         self.file: os.PathLike = file
@@ -15,20 +15,5 @@ class Component():
 
     def add_Component(self, component):
         self.components.append(component)
-
-
-class Page():
-    def __init__(self, name, file: os.PathLike):
-        self.name = name
-        self.file: os.PathLike = file
-        self.components = []
-
-    def render(self):
-        file = open(str(self.file), "r")
-        renderderer = file.read()
-        for component in self.components:
-            renderderer = renderderer.replace(''.join(["<", "component:", component.name, "/>"]), component.render())
-        return renderderer
-
-    def add_Component(self, component: Component):
-        self.components.append(component)
+class Page(Component):
+    pass
